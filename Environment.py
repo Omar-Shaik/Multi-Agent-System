fa4e9ae83c0a71c2ffb23d96f570e7697a5c6f9c
Oshaik <35842212+Oshaik@users.noreply.github.com>
2018-03-25 20:18:06 -0400

    Update Environment.py

--- Environment.py
+++ Environment.py
@@ -10,8 +10,22 @@ class Environment:
         self.objects = []
         self.public_channel = CommunicationChannel(0) #for some reason this isn't working
 
-
-
+    #Function checks if given position is unoccupied and within bounds
+    #validity variable holds the validity of the position. Only changes if position is invalid
+    #loop variable controls the entry of the loop for secondary verification. Avoids unneccessary entry if first condition makes the position invalid
+    def validPosition(self, x, y):
+        validity = True
+        loop = True
+        if self.x_upper <= x <= self.x_lower: #Check in bounds
+            if self.y_upper <= y <= self.y_lower:
+                validity = False
+                loop = False
+        if (loop):
+            for object in self.objects: #Check unoccupied
+                if object.x_pos == x and object.y_pos == y:
+                    validity = False
+        return validity
+    
     #Registers an object in the environment
     #Uses the validPosition function
     def registerObject(self, object):
@@ -21,38 +35,15 @@ class Environment:
         else:
             return False
 
-    #Function checks if given position is unoccupied and within bounds
-    #validity variable holds the validity of the position. Only changes if position is invalid
-    #loop variable controls the entry of the loop for secondary verification. Avoids unneccessary entry if first condition makes the position invalid
-    def validPosition(self, x, y):
-        validity = True
-        loop = True
-
-        if self.x_upper <= x <= self.x_lower: #Check in bounds
-            if self.y_upper <= y <= self.y_lower:
-                validity = False
-                loop = False
-
-        if (loop):
-            for object in self.objects: #Check unoccupied
-                if object.x_pos == x and object.y_pos == y:
-                    validity = False
-
-        return validity
-
-
     #Get a list of elements visible from the given coordinates
     def objectsAround(self, controller):
         visible = []
-
-        #Euclidean distance: distance^2 = xdiff^2 + ydiff^2
         for object in self.objects:
-            #If agent not at scan position AND Euclidean distance less than/equal to 10
             if math.sqrt((controller.body.x_pos - object.x_pos)**2 + (controller.body.y_pos - object.y_pos)**2 ) <= 10 and object.getObjecType() == 0:
                 visible.append(object)
-
         return visible
-
+    
+    #Function allows an agent to pick up a target if that target is within its range
     def pickUp(self, controller, target):
         if target in self.objects and controller.body.target_type == target.target_type:
             self.objects.remove(target)
