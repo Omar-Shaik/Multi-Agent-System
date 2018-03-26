7776e02849f71910fbcc01cb1e04a50224d3d280
Oshaik <35842212+Oshaik@users.noreply.github.com>
2018-03-25 21:08:09 -0400

    Update Environment.py

--- Environment.py
+++ Environment.py
@@ -34,30 +34,23 @@ class Environment:
     def validPosition(self, x, y):
         validity = True
         loop = True
-        
         if self.x_upper <= x <= self.x_lower: #Check in bounds
             if self.y_upper <= y <= self.y_lower:
                 validity = False
                 loop = False
-
         if (loop):
             for object in self.objects: #Check unoccupied
                 if (object.x_pos == x and object.y_pos == y) or (object.object_type == 1 and math.sqrt((object.x_pos - x)**2 + (object.y_pos - y)**2)) < 20:
                     validity = False
-
         return validity
 
 
     #Get a list of elements visible from the given coordinates
     def objectsAround(self, controller):
         visible = []
-
-        #Euclidean distance: distance^2 = xdiff^2 + ydiff^2
         for object in self.objects:
-            #If agent not at scan position AND Euclidean distance less than/equal to 10
             if math.sqrt((controller.body.x_pos - object.x_pos)**2 + (controller.body.y_pos - object.y_pos)**2 ) <= 10 and object.getObjecType() == 0:
                 visible.append(object)
-
         return visible
 
     def pickUp(self, controller, target):
