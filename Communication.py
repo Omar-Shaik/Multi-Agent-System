4c51569cf4c262251aa83ccb9f518eb7d9f48c30
Oshaik <35842212+Oshaik@users.noreply.github.com>
2018-03-27 15:04:24 -0400

    This is done. Someone double check.

--- Communication.py
+++ Communication.py
@@ -1,18 +1,30 @@
 class CommunicationChannel:
-
+    
+    '''
+    Initialization function requires just the channel type.
+    Public channels have type 0. Private channels have type 1.
+    The access list holds all agents that have access to the channel.
+    The message log holds all previous messages.
+    '''
     def __init__(self, type):
-        #Public channels are type 0. Private channels are type 1.
         self.channel_type = type
         self.access_list = []
         self.message_log = []
-
-    #Only the public channel can have unlimited accesssors added to it
-    #Private channels between two agents can't have more accessors added to them
+    
+    '''
+    Only public channels can have unlimited accessors in it.
+    Private channels cannot be formed between more than two agents.
+    '''
     def addAccess(self, accessor):
         if (self.channel_type == 0 or len(self.access_list) < 2):
             self.access_list.append(accessor)
 
-    #Each controller has a list of new messages
+    '''
+    A message is sent by specifying the sender and the message.
+    Message is only sent if sender is in the access list.
+    Message is added to the message log.
+    Message is added to the new messages list of every agent besides the sender in the access list.
+    '''
     def sendMessage(self, sender, message):
         if sender in self.access_list:
             # Adds message to message log
