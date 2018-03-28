8fa9bc33fd38952de510ee1473b98ea56d321e23
Oshaik <35842212+Oshaik@users.noreply.github.com>
2018-03-27 23:14:06 -0400

    Update System.py

--- System.py
+++ System.py
@@ -1,10 +1,15 @@
 import Environment as env
 import Agent as agt
 
-class System:
+class MultiAgentSystem:
 
-    def __int__(self, scenario, x_upper, x_lower, y_upper, y_lower, agents):
+    def __int__(self, scenario, x_upper, x_lower, y_upper, y_lower, number_of_agents, targets_per_agent):
 
         self.scenarios = ["Competitive", "Compassionate", "Collaborative"]
-        self.scenario_type = self.scenarios.index(scenario)
+        self.scenario_type = self.scenarios.index(scenario) 
         self.environment = env.Environment(x_upper, x_lower, y_upper, y_lower)
+        self.environment.populate(number_of_agents, targets_per_agent)
+        
+        
+    def startSystem(self):
+        #simulate
