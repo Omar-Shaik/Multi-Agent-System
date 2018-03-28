4688771f9e1d7c74c12fe423ab4ca0cc5b34568a
Oshaik <35842212+Oshaik@users.noreply.github.com>
2018-03-28 09:52:16 -0400

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,46 +1,18 @@
 class Controller:
 
-    def __init__(self, body):
+    def __init__(self, body, type):
         self.up = [0, 1]
         self.right = [1, 0]
         self.down = [0, -1]
         self.left = [-1, 0]
+        self.type = type
         self.body = body
         self.body.controller = self 
     
     def scanner(self):
         return self.body.env.objectsAround(self)
-
-
-#Each type of controller will do something differnt with the list of objects around it
-class CompetitiveController(Controller):
+    
+    def radar(self):
         
-        def __init__(self):
-        
-        
-        def radar(self):
-            
-            
-        def nextMove(self):       
-        
-class CollaborativeController(Controller):
-
-        def __init__(self):
-        
-        
-        def radar(self):
-            
-        
-        def nextMove(self)
-        
-
-class CompassionateController(Controller):
-        
-        def __int__(self):
-        
-        
-        def radar(self):
-            
-        
-        def nextMove(self):
+    def nextMove(self):
         
