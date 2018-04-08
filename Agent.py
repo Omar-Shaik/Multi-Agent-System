ea55c5603dcb3d706392297d170ea00976b485c2
Oshaik <35842212+Oshaik@users.noreply.github.com>
2018-04-08 11:28:53 -0400

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -6,24 +6,9 @@ class Agent:
     def __init__(self, env, x, y, target_type, controller_type):
         self.body = Object.Body(x, y, target_type)
         if controller_type == 0:
-            self.controller = Controller.Competitive_Controller(self.body)
+            self.controller = Controller.Competitive_Controller(self.body, env)
         elif controller_type == 1:
-            self.controller = Controller.Collaborative_Controller(self.body)
+            self.controller = Controller.Collaborative_Controller(self.body, env)
         elif controller_type == 2:
-            self.controller = Controller.Compassionate_Controller(self.body)
+            self.controller = Controller.Compassionate_Controller(self.body, env)
         self.body.controller = self.controller
-        self.channels = []
-        self.new_messages = []
-        self.collected_count = 0
-        self.heading = []
-        self.visited = []
-        self.next_move = []
-
-    def sendMessage(self, channel, message):
-        channel.sendMessage(self, message)
-
-    def readNewMessage(self):
-        for i in self.new_messages:
-        # read message
-        self.new_messages.
-        del (i)
