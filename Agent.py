beded251c7adc362ab81dd405b55a38688cb17fd
Oshaik <35842212+Oshaik@users.noreply.github.com>
2018-04-08 11:53:46 -0400

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -12,4 +12,3 @@ class Agent:
         elif controller_type == 2:
             self.controller = Controller.Compassionate_Controller(self.body, env)
         self.body.controller = self.controller
-        self.channels = []
