adf31f4ba9b86e181eef726dc4f010d6a8331ddc
Oshaik <35842212+Oshaik@users.noreply.github.com>
2018-04-08 11:27:45 -0400

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,11 +1,12 @@
 class Controller:
-    def __init__(self, body):
+    def __init__(self, body, env):
         self.up = [0, 1]
         self.right = [1, 0]
         self.down = [0, -1]
         self.left = [-1, 0]
         self.collected = 0
         self.body = body
+        self.environment = env
         self.body.controller = self
 
     def scanner(self):
@@ -18,17 +19,16 @@ class Controller:
         return 0
 
 class Competitive_Controller(Controller):
-    def __init__(self, body):
-        Controller.__init__(self, body)
+    def __init__(self, body, env):
+        Controller.__init__(self, body, env)
         x = 1
 
 class Collaborative_Controller(Controller):
-    def __init__(self, body):
-        Controller.__init__(self, body)
+    def __init__(self, body, env):
+        Controller.__init__(self, body, env)
         x = 1
 
 class Compassionate_Controller(Controller):
-    def __init__(self, body):
-        Controller.__init__(self, body)
+    def __init__(self, body, env):
+        Controller.__init__(self, body, env)
         x = 1
-        
