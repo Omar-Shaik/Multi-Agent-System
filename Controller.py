f00e83cccffe1ef40ea4b08346bfed5213b50508
Oshaik <35842212+Oshaik@users.noreply.github.com>
2018-04-08 11:20:42 -0400

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,18 +1,34 @@
 class Controller:
-
-    def __init__(self, body, type):
+    def __init__(self, body):
         self.up = [0, 1]
         self.right = [1, 0]
         self.down = [0, -1]
         self.left = [-1, 0]
-        self.type = type
+        self.collected = 0
         self.body = body
-        self.body.controller = self 
-    
+        self.body.controller = self
+
     def scanner(self):
         return self.body.env.objectsAround(self)
-    
+
     def radar(self):
-        
+        return 0
+
     def nextMove(self):
+        return 0
+
+class Competitive_Controller(Controller):
+    def __init__(self, body):
+        Controller.__init__(self, body)
+        x = 1
+
+class Collaborative_Controller(Controller):
+    def __init__(self, body):
+        Controller.__init__(self, body)
+        x = 1
+
+class Compassionate_Controller(Controller):
+    def __init__(self, body):
+        Controller.__init__(self, body)
+        x = 1
         
